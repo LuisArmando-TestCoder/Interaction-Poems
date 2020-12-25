2e9c3f1991a4747f0da6ec8c42f3feb44d607660
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-12-25 16:25:21 -0600

    enhance keyboard styles

--- src/components/Key/index.tsx
+++ src/components/Key/index.tsx
@@ -3,15 +3,15 @@ import { events } from '../../scenePreset'
 import './style.scss'
 
 const index = ({ keyName }) => {
-    const [isSelected, setIsSelected] = useState(null)
+    const [isSelected, setIsSelected] = useState(false)
     
     useEffect(() => {
         events.onKey(keyName).start(() => setIsSelected(true))
-        events.onKey(keyName).end(() => setIsSelected(null))
+        events.onKey(keyName).end(() => setIsSelected(false))
     }, [])
 
     return <button className={
-        `key ${ keyName } ${ isSelected && 'selected' }`
+        `key ${ keyName } ${ isSelected ? 'selected' : '' }`
     }>{ keyName.toUpperCase() }</button>
 }
 
